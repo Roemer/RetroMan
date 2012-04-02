408e43135419766a7e1f7b8b315a379b5f0c4f55
r.baeriswyl@gmail.com <r.baeriswyl@gmail.com@0600100e-e6de-35bc-fb51-5806e8a3db2b>
2012-04-02 22:19:20 +0000

    - Added some Models and Serialization Objects

---
+++ trunk/Source/RetroMan/Database/FileObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RetroMan.Database
+{
+    [Serializable]
+    public class FileObject
+    {
+        public string Name { get; set; }
+        public FileType FileType { get; set; }
+        public long FileSize { get; set; }
+        public int CRC { get; set; }
+        public Guid MD5 { get; set; }
+        public string SHA1 { get; set; }
+
+        public FileObject()
+        {
+        }
+    }
+}
